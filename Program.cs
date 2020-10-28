bb99340e40de306f4f2cdfd12c68a1c1060a44ed
Erik Hålenius <erik.halenius@54u.se>
2020-10-28 18:48:12 +0100

    update with correct template

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace blazor_test
 {
@@ -14,7 +17,7 @@ namespace blazor_test
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
